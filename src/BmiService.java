648d4e3e87d94f0c2471c56d9ac87124262f8089
Anna-Filipova <62184331+Anna-Filipova@users.noreply.github.com>
2020-04-07 13:35:19 +0300

    Update BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,9 +1,8 @@
 public abstract class BmiService<height mass> {
-    public abstract int calculate(int mass; int height);
+    public abstract int calculate(int mass;int height);
+
     int index;
-    {
-        index = mass / (height * height);
+    index =mass /(height *height);
         System.out.println(index);
-    }
         return index;
 }
