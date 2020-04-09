ce11f2a94f8d6555b38d10ea91d9f263ea4f176d
Anna-Filipova <62184331+Anna-Filipova@users.noreply.github.com>
2020-04-09 21:31:40 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int index = service.calculate(mass height);
+        double mass = 65;
+        double height = 166;
+        double index = service.calculate(mass, height);
         System.out.println(index);
     }
 }
